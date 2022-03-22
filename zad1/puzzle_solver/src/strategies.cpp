bff0ca659eb37349104853034cdbe8490d7eb106
Firedog01 <--global>
2022-03-22 20:51:09 +0100

    dfs implemented but loops indefinitely. Max depth required

--- zad1/puzzle_solver/src/strategies.cpp
+++ zad1/puzzle_solver/src/strategies.cpp
@@ -1,3 +1,4 @@
+#include <stack>
 #include "../lib/strategies.h"
 
 strategies::strategies() {
@@ -12,19 +13,20 @@ strategies::~strategies() {
 }
 
 op_path strategies::bfs(state &start_state, ops::operators *order, info_bundle &info) {
-    std::queue<state> open_states;
-    std::unordered_map<board, op_path, board_hash> processed_states;
-    state* cur_state;   // for iteration
-    open_states.emplace(start_state);                     /// Q.enqueue(s)
-
     info.processed++;
     info.visited++;
 
-    while(!open_states.empty()) {                          /// while !Q.isempty():
-        cur_state = &open_states.front();                  /// v = Q.dequeue()
+    std::queue<state> open_states;                        /// Q - queue
+    std::unordered_map<board, op_path, board_hash> processed_states; /// U - set
+    state* cur_state;   // for iteration
+
+    open_states.push(start_state);                     /// Q.enqueue(s)
+
+    while(!open_states.empty()) {                         /// while !Q.isempty():
+        cur_state = &open_states.front();                 /// v = Q.dequeue()
 
         ops::operators* op = order;
-        for(int i = 0; i < 4; i++, op++) {                  /// for n in neighbours(v):
+        for(int i = 0; i < 4; i++, op++) {                /// for n in neighbours(v):
             auto neighbour = board_handler::new_moved(*cur_state, *op); // uses new, must be deleted
             if(neighbour == nullptr) { // illegal move or trivial(for example RL or UD)
                 continue;
@@ -32,9 +34,10 @@ op_path strategies::bfs(state &start_state, ops::operators *order, info_bundle &
             info.set_max_depth(neighbour->second.len);
             if(board::same(solved_table, neighbour->first.table.data())) { /// if n is solution:
                 // solution found!
+				info.visited++; //xxx not sure if right
                 op_path solution = neighbour->second;
                 delete neighbour;
-                return solution;
+                return solution;						/// return success
             }
 
             /// if !U.has(n):
@@ -56,8 +59,42 @@ op_path strategies::bfs(state &start_state, ops::operators *order, info_bundle &
 }
 
 op_path strategies::dfs(state &start_state, ops::operators *order, info_bundle &info) {
-    if(board::same(start_state.first.table.data(), solved_table)) {
-
+    info.processed++;
+    info.visited++;
+    if(board::same(start_state.first.table.data(), solved_table)) { /// if s is solution:
+        return {0};										/// return success
     }
+    std::stack<state> open_states;							/// S - stack
+    std::unordered_map<board, op_path, board_hash> processed_states; /// T - set
+
+    state* cur_state;   // for iteration
+
+    open_states.push(start_state);						/// S.push(s)
+    while(!open_states.empty()) {
+		info.processed++;
+        cur_state = &open_states.top();						/// v = S.pop()
+		auto it = processed_states.insert(*cur_state);
+		if(it.second) { // insertion successful
+			ops::operators* op = order;
+			op += 4;
+			for(; op != order; op--) {    					/// for n in neighbours(v).reverse():
+				auto neighbour = board_handler::new_moved(*cur_state, *op); // uses new, must be deleted
+				if(neighbour == nullptr) { // illegal move or trivial(for example RL or UD)
+					continue;
+				}
+				info.visited++;
+				info.set_max_depth(neighbour->second.len);
+				if(board::same(solved_table, neighbour->first.table.data())) { /// if n is solution:
+					// solution found!
+					op_path solution = neighbour->second;
+					delete neighbour;
+					return solution;						/// return success
+				}
+				open_states.push(*neighbour); 			/// S.push(n)
+				delete neighbour;
+			}
+		}
+    }
+	return {-1};											/// return failure
 }
 
