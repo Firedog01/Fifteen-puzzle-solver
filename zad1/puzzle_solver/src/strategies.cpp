27e7aefcb3fb7160e6310ee58298e185229ef779
LiptonLeon <krystian.baraniecki@gmail.com>
2022-05-14 18:30:45 +0200

    Fixed counting processed in BFS

--- zad1/puzzle_solver/src/strategies.cpp
+++ zad1/puzzle_solver/src/strategies.cpp
@@ -41,6 +41,7 @@ op_path strategies::bfs(state &start_state, ops::operators *order, info_bundle &
     while(!open_states.empty()) {                         /// while !Q.isempty():
         cur_state = &open_states.front();                 /// 	v = Q.dequeue()
         ops::operators* op = order;
+        info.processed++;
         for(int i = 0; i < 4; i++, op++) {                /// for n in neighbours(v):
 			state* neighbour = board_handler::new_moved(*cur_state, *op); // uses new, must be deleted
 			if(neighbour == nullptr)  // illegal move or trivial(for example RL or UD)
@@ -65,7 +66,6 @@ op_path strategies::bfs(state &start_state, ops::operators *order, info_bundle &
             }
             delete neighbour;
         }
-        info.processed++;
         open_states.pop();
     }
     return {ops::NotFound}; // will display size of -1
